1f624dda760f55bf3d9545117197ba5964a55f44
Gürhan Küçük <gurhan.kucuk@yandex.com>
2018-08-16 22:24:44 +0300

    delete song added.

--- src/app/pages/detail/detail.page.ts
+++ src/app/pages/detail/detail.page.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Song} from '../../models/song.interface';
+import {FirestoreService} from '../../services/data/firestore.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AlertController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-detail',
-  templateUrl: './detail.page.html',
-  styleUrls: ['./detail.page.scss'],
+    selector: 'app-detail',
+    templateUrl: './detail.page.html',
+    styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
 
-  constructor() { }
+    public song: Observable<Song>;
 
-  ngOnInit() {
-  }
+    constructor(private firestoreService: FirestoreService,
+                private route: ActivatedRoute,
+                private alertController: AlertController,
+                private router: Router) {
+    }
+
+    ngOnInit() {
+        const songId = this.route.snapshot.paramMap.get('id');
+        this.song = this.firestoreService.getSongDetail(songId).valueChanges();
+    }
+
+    async deleteSong() {
+        const songId = this.route.snapshot.paramMap.get('id');
+        const alert = await this.alertController.create({
+            message: 'Are you sure you want to delete the song?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'Cancel',
+                    handler: blah => {
+                        console.log('Confirm Cancel: blah');
+                    }
+                },
+                {
+                    text: 'Okay',
+                    handler: () => {
+                        this.firestoreService.deleteSong(songId).then(() => {
+                            this.router.navigateByUrl('');
+                        });
+                    }
+                }
+            ]
+        });
+        await alert.present();
+    }
 
 }
--- src/app/services/data/firestore.service.ts
+++ src/app/services/data/firestore.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 import {Song} from '../../models/song.interface';
 
 @Injectable({
@@ -27,5 +27,13 @@ export class FirestoreService {
 
     getSongList(): AngularFirestoreCollection<Song> {
         return this.firestore.collection(`songList`);
+    }
+
+    getSongDetail(songId: string): AngularFirestoreDocument<Song> {
+        return this.firestore.collection('songList').doc(songId);
+    }
+
+    deleteSong(songId: string): Promise<void> {
+        return this.firestore.doc(`songList/${songId}`).delete();
     }
 }
